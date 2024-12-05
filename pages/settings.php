a8beb9f00a9c597f7130d325b47c666058bfb405
Nishaanjalin2023 <139215902+Nishaanjalin2023@users.noreply.github.com>
2024-12-05 23:58:08 +0000

    connect setting

--- pages/settings.php
+++ pages/settings.php
@@ -3,7 +3,7 @@
 $servername = "27.0.0.1:3306";
 $username = "root";
 $password = "mariadb";
-$dbname = "Lab";
+$dbname = "timetable_management";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
