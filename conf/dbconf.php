a2b4d2b6294a29819b9942339b1036f600b1a825
Nishaanjalin2023 <139215902+Nishaanjalin2023@users.noreply.github.com>
2024-12-06 10:39:37 +0000

    connection file created

--- conf/dbconf.php
+++ conf/dbconf.php
@@ -3,7 +3,7 @@
 define('SERVERNAME', '127.0.0.1:3306');
 define('USERNAME', 'root');
 define('PASSWORD', 'mariadb');
-define('DBNAME', 'crea');
+define('DBNAME', 'timetable_management');
 try{
 	//connect with database
 $connect = mysqli_connect(SERVERNAME,USERNAME,PASSWORD,DBNAME);
